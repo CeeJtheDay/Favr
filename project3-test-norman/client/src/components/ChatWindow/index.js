62660b29270f6926897cfe62179738c6ad393aa2
Norman Lei <normanlei0901@gmail.com>
2020-03-04 17:06:00 -0800

    norman update chat styling

--- project3-test-norman/client/src/components/ChatWindow/index.js
+++ project3-test-norman/client/src/components/ChatWindow/index.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import $ from "jquery";
-// import Script from 'react-load-script';
 import socketIOClient from "socket.io-client";
-import Alert from "../Alert";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import ListIcon from "@material-ui/icons/List";
+import IconButton from "@material-ui/core/IconButton";
+import Modal from "../Modal";
+import Popover from '@material-ui/core/Popover';
+import Alert from '../Alert';
+import "./chat-style.css";
 
-const ChatWindow = ({ chatRoom, currUser }) => {
+const ChatWindow = ({ chatRoom, currUser, toggleDrawer, state, sideList, modalState, modalStyle, handleClose }) => {
     const socket = socketIOClient();
     const [imagePath, setImagePath] = useState("");
     const [imagedata, setImageData] = useState({
@@ -12,91 +17,168 @@ const ChatWindow = ({ chatRoom, currUser }) => {
         error: ""
     });
 
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClosePop = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
     function loadSocket() {
         // join
         socket.on('connect', function () {
             socket.emit('join', currUser.id, chatRoom.id, currUser.name);
         })
-        socket.once('history', function (history) {
-            console.log(history);
-            for (var i = 0; i < history.length; i++) {
-                var message = 
-                    `<div class="message">
-                    <span class="user">${history[i].userName}: </span>
-                    <span class="msg">${history[i].message}</span>
-                    </div>
-                    <div class="sysMsg">${history[i].time}</div>`;
-                $('#msglog').append(message);
-                $('#msglog').scrollTop($('#msglog')[0].scrollHeight);
-            }
-        })
-
-        // listen
-        socket.once('sys', function (sysMsg) {
-            var message = '<div class="sysMsg">' + sysMsg + '</div>';
-            $('#msglog').append(message);
-
-        });
-
-        // send
-        socket.on('msg', function (userName, msg, time) {
-            let acknowledged = JSON.parse(localStorage.getItem("socketDup"));
-
-            if (!~acknowledged.indexOf(msg.event_id)) {
-
-                // add to array of acknowledged events
-                acknowledged.unshift(msg.event_id);
-
-                // prevent array from growing to large
-                if (acknowledged.length > 20) {
-                    acknowledged.length = 20;
-                }
-
-                localStorage.setItem("socketDup", JSON.stringify(acknowledged));
-
-                let message;
-                if (!msg.image) {
-                    message =
-                        `<div class="message">
-                        <span class="user bold">${userName}</span>
-                        <span class="sysMsg">${time}</span>
-                        <br/>
-                        <span class="msg">${msg.msg}</span>
-                        </div>`;
-                } else {
-                    message = `<div class='message'>
-                    <span class='user bold'>${userName}</span>
-                    <span class='sysMsg'>${time}</span>
-                    <br/>
-                    <img src='/uploads/${msg.image}' height='100' width='100'>
-                    <br/>
-                    <span class='msg'>${msg.msg}</span>
-                    </div>`;
-                }
-                $('#msglog').append(message);
-                $('#msglog').scrollTop($('#msglog')[0].scrollHeight);
-            }
-        });
 
         // message
 
-        $(`#${chatRoom.id}${currUser.id}`).keydown(function (e) {
-            if (e.which === 13 && $(this).val()) {
-                e.preventDefault();
-                let msg = $(this).val();
-                $(this).val('');
-                console.log(imagePath);
-                let hash = currUser.name + chatRoom.id + currUser.id;
-                console.log(hash);
-                socket.send(msg, chatRoom.id, currUser.id, currUser.name, hash, imagePath);
-            }
-        });
+        // $(`#${chatRoom.id}${currUser.id}`).keydown(function (e) {
+        //     if (e.which === 13 && $(this).val()) {
+        //         e.preventDefault();
+        //         let msg = $(this).val();
+        //         $(this).val('');
+        //         console.log(imagePath);
+        //         let hash = currUser.name + chatRoom.id + currUser.id;
+        //         console.log(hash);
+        //         console.log(chatRoom.id);
+        //         socket.send(msg, chatRoom.id, currUser.id, currUser.name, hash, imagePath);
+        //     }
+        // });
 
 
     }
 
+    //socket listener
+
+    socket.once('history', function (history) {
+        for (var i = 0; i < history.length; i++) {
+            let message;
+            if (history[i].id === currUser.id) {
+                message = `
+                <div class="d-flex justify-content-end mb-4">
+                    <div class="msg_cotainer_send">
+                        ${history[i].message}
+                        <span class="msg_time_send">${history[i].time}</span>
+                    </div>
+                    <div class="img_cont_msg">
+                        <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg" />
+                    </div>
+                </div>`
+            } else {
+                message = `
+                <div class="d-flex justify-content-start mb-4">
+                    <div class="img_cont_msg">
+                        <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg" />
+                    </div>
+                    <div class="msg_cotainer">
+                        ${history[i].message}
+                        <span class="msg_time">${history[i].time}</span>
+                    </div>
+                 </div>
+                `
+            }
+            $('#msglog').append(message);
+            $('#msglog').scrollTop($('#msglog')[0].scrollHeight);
+        }
+    })
+
+    // listen
+    socket.once('sys', function (sysMsg) {
+        var message = '<div class="sysMsg mb-4" style="text-align:center;background:lightgray">' + sysMsg + '</div>';
+        $('#msglog').append(message);
+
+    });
+
+    // send
+    socket.on('msg', function (userName, msg, msgObj, time) {
+        let acknowledged = JSON.parse(localStorage.getItem("socketDup"));
+        console.log(acknowledged);
+        console.log(acknowledged.indexOf(msg.event_id));
+        if (!~acknowledged.indexOf(msg.event_id)) {
+
+            // add to array of acknowledged events
+            acknowledged.unshift(msg.event_id);
+
+            // prevent array from growing to large
+            if (acknowledged.length > 20) {
+                acknowledged.length = 20;
+            }
+
+            localStorage.setItem("socketDup", JSON.stringify(acknowledged));
+
+            let message;
+            console.log(!msg.image);
+            if (!msg.image) {
+                if (msgObj.id === currUser.id) {
+                    message = `
+                <div class="d-flex justify-content-end mb-4">
+                    <div class="msg_cotainer_send">
+                            ${msg.msg}
+                        <span class="msg_time_send">${time}</span>
+                    </div>
+                    <div class="img_cont_msg">
+                        <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg" />
+                    </div>
+                </div>
+                `;
+                } else {
+                    message = `
+                    <div class="d-flex justify-content-start mb-4">
+                        <div class="img_cont_msg">
+                            <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg" />
+                        </div>
+                        <div class="msg_cotainer">
+                            ${msg.msg}
+                            <span class="msg_time">${time}</span>
+                        </div>
+                    </div>
+                    `;
+                }
+            } else {
+                if (msgObj.id === currUser.id) {
+                    message = `
+                    <div className="d-flex justify-content-end mb-4">
+                        <div className="msg_cotainer_send">
+                            <img src='/uploads/${msg.image}' height='100' width='100'><br/>
+                                ${msg.msg}
+                            <span className="msg_time_send">${time}</span>
+                        </div>
+                        <div className="img_cont_msg">
+                            <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" className="rounded-circle user_img_msg" />
+                        </div>
+                    </div>
+                    `
+                } else {
+                    message = `
+                    <div class="d-flex justify-content-start mb-4">
+                        <div class="img_cont_msg">
+                            <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg" />
+                        </div>
+                        <div class="msg_cotainer">
+                            <img src='/uploads/${msg.image}' height='100' width='100'><br/>
+                            ${msg.msg}
+                            <span class="msg_time">${time}</span>
+                        </div>
+                    </div>
+                    `;
+                }
+            }
+            $('#msglog').append(message);
+            $('#msglog').scrollTop($('#msglog')[0].scrollHeight);
+        }
+    });
+
+
+
     useEffect(() => {
         $('#msglog').empty();
+        console.log("here");
         loadSocket();
 
     }, [chatRoom])
@@ -110,6 +192,17 @@ const ChatWindow = ({ chatRoom, currUser }) => {
         let hash = currUser.name + chatRoom.id + currUser.id;
         console.log(hash);
         socket.send(msg, chatRoom.id, currUser.id, currUser.name, hash, imagePath);
+    }
+
+    const handSendEnter = e => {
+        if (e && e.type === "keydown" && e.keyCode === 13) {
+            let msg = $(`#${chatRoom.id}${currUser.id}`).val().trim();
+            $(`#${chatRoom.id}${currUser.id}`).val('');
+            console.log(imagePath);
+            let hash = currUser.name + chatRoom.id + currUser.id;
+            console.log(hash);
+            socket.send(msg, chatRoom.id, currUser.id, currUser.name, hash, imagePath);
+        }
     }
 
     const handleSubmitImage = event => {
@@ -143,7 +236,86 @@ const ChatWindow = ({ chatRoom, currUser }) => {
 
 
     return (
-        <div>
+        <div style={{ height: "90vh" }}>
+            <div className="card" style={{ minHeight: "100%" }}>
+                <div className="card-header msg_head">
+                    <div className="d-flex bd-highlight">
+                        <div className="img_cont">
+                            <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" className="rounded-circle user_img" />
+                            <span className="online_icon"></span>
+                        </div>
+                        <div className="user_info">
+                            <span>Chat with {chatRoom.other.name}</span>
+                        </div>
+                    </div>
+                    <span id="action_menu_btn">
+                        <IconButton onClick={toggleDrawer("left", true)}>
+                            <ListIcon />
+                        </IconButton>
+                    </span>
+                </div>
+                <div className="card-body msg_card_body" id="msglog">
+                </div>
+                <div className="card-footer">
+                    <div className="input-group">
+                        <div className="input-group-append">
+                            <span className="input-group-text attach_btn" aria-describedby={id} onClick={handleClick}><i className="fas fa-paperclip"></i></span>
+                            <Popover
+                                id={id}
+                                open={open}
+                                anchorEl={anchorEl}
+                                onClose={handleClosePop}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'center',
+                                }}
+                                transformOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'center',
+                                }}
+                            >
+                                <form id="imageSubmit" action="/upload" method="POST" encType="multipart/form-data" onSubmit={handleSubmitImage}>
+                                    <div className="form-group">
+                                        <label htmlFor="pic">Upload Profile Image:</label>
+                                        <input type="file" className="form-control-file" name="userImage" id="upload" onChange={clearAlert}></input>
+                                    </div>
+                                    <button className="btn btn-success" type="submit">Upload(Upload Before Submit)</button>
+                                    <Alert type="danger" style={{ display: imagedata.error ? 'block' : 'none', marginBottom: 10 }}>
+                                        {imagedata.error}
+                                    </Alert>
+                                    <Alert type="success" style={{ display: imagedata.success ? 'block' : 'none', marginBottom: 10 }}>
+                                        {imagedata.success}
+                                    </Alert>
+                                </form>
+                            </Popover>
+                        </div>
+                        <textarea name="" className="form-control type_msg" placeholder="Type your message..." id={chatRoom.id + currUser.id} onKeyDown={handSendEnter}></textarea>
+                        <div className="input-group-append" onClick={handSend}>
+                            <span className="input-group-text send_btn"><i className="fas fa-location-arrow"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <SwipeableDrawer
+                open={state.left}
+                onClose={toggleDrawer("left", false)}
+                onOpen={toggleDrawer("left", true)}
+            >
+                {sideList("left")}
+            </SwipeableDrawer>
+            <Modal open={modalState.open} handleClose={handleClose} modalStyle={modalStyle} reviewer={currUser.id} reviewee={modalState.reviewee} />
+        </div>
+
+    )
+}
+
+
+export default ChatWindow;
+
+
+
+{/* <div>
             <h3>chatRoom#: {chatRoom.id}</h3>
             <div class="container-fluid justify-content-center">
                 <div class="chatBox col" id="msglog">
@@ -164,9 +336,4 @@ const ChatWindow = ({ chatRoom, currUser }) => {
                 </form>
                 <div><button onClick={handSend}>Submit</button></div>
             </div>
-        </div>
-    )
-}
-
-
-export default ChatWindow;
+        </div> */}
--- project3-test-norman/client/src/pages/Chat.js
+++ project3-test-norman/client/src/pages/Chat.js
@@ -1,30 +1,44 @@
 import React, { useState, useEffect } from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import { withRouter } from 'react-router-dom';
 import API from "../utils/API-Barter";
-import ChatList from "../components/ChatList";
-// import APIU from "../utils/API-User";
 import axios from 'axios';
 import ChatWindow from "../components/ChatWindow";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import ListIcon from "@material-ui/icons/List";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from '@material-ui/icons/Delete';
+import RateReviewIcon from '@material-ui/icons/RateReview';
+import "./chat-style.css";
 import $ from "jquery";
-// import { useTheme } from '@material-ui/core/styles';
+import Modal from "../components/Modal";
 
-// const useStyles = makeStyles(theme => ({
-//     root: {
-//         flexGrow: 1,
-//     },
 
-// }));
+const useStyles = makeStyles({
+    list: {
+        width: 300
+    },
+});
 
 const Chat = withRouter(({ history, currUser, setCurrUser }) => {
-    // const theme = useTheme();
-    // const classes = useStyles();
+    const classes = useStyles();
+    const [modalStyle] = useState(getModalStyle);
+    function getModalStyle() {
+        const top = 50;
+        const left = 50;
+
+        return {
+            top: `${top}%`,
+            left: `${left}%`,
+            transform: `translate(-${top}%, -${left}%)`,
+        };
+    }
+
     const [state, setState] = useState({
         chatList: [],
         currChat: {},
+        left: false,
     })
-    console.log(state.currChat);
 
     const handleName = (currList) => {
         let tempChatList = [];
@@ -40,13 +54,13 @@ const Chat = withRouter(({ history, currUser, setCurrUser }) => {
             let otherId = chat.user2 === currUser.id ? chat.user1 : chat.user2;
             let tempChatObj = {
                 id: chat._id,
-                self: { id: selfId, name: currUser.name },
+                self: { id: selfId, name: currUser.name },//add image here later
                 other: { id: otherId }
             };
             axios.get(`../api/users/${otherId}`)
                 .then(data1 => {
                     console.log(data1.data);
-                    tempChatObj.other.name = data1.data.name;
+                    tempChatObj.other.name = data1.data.name; //add image here later
                     tempChatList.push(tempChatObj);
                     console.log(tempChatList);
                     if (tempChatList.length === currList.length) {
@@ -73,19 +87,104 @@ const Chat = withRouter(({ history, currUser, setCurrUser }) => {
             })
     }, [])
 
+    //modal handler
+    const [modalState, setModalState] = useState({
+        open: false,
+        reviewee: ""
+    });
+    const handleOpen = (reviewee) => {
+        setModalState({
+            open: true,
+            reviewee: reviewee
+        });
+    };
 
-    return (
-        <div>
-            <Grid container spacing={2}>
-                <Grid item xs={4}>
-                    <ChatList chatList={state.chatList} state={state} setState={setState} currUser={currUser} setCurrUser={setCurrUser} />
-                </Grid>
-                <Grid item xs={8}>
-                    {!$.isEmptyObject(state.currChat) && (<ChatWindow chatRoom={state.currChat} currUser={currUser} />)}
-                </Grid>
-            </Grid>
+    const handleClose = () => {
+        setModalState({
+            open: false,
+            reviewee: ""
+        });
+    };
+
+    //sidedrwaer handler
+    const toggleDrawer = (side, open) => event => {
+        if (
+            event &&
+            event.type === "keydown" &&
+            (event.key === "Tab" || event.key === "Shift")
+        ) {
+            return;
+        }
+
+        console.log(side);
+
+        setState({ ...state, [side]: open });
+    };
+
+    const handleOpenChatWindow = tile => {
+        console.log(tile);
+        setState({ ...state, currChat: tile, left: false })
+    };
+
+    const sideList = side => (
+        <div
+            className={classes.list}
+            role="presentation"
+            //onClick={toggleDrawer(side, false)}
+            //onKeyDown={toggleDrawer(side, false)}
+        >
+            <div className="card contacts_card">
+                <div className="card-body contacts_body">
+                    <ui className="contacts">
+                        {state.chatList.map((tile) => (
+                            <li className="active" onClick={() => handleOpenChatWindow(tile)}>
+                                <div className="d-flex bd-highlight">
+                                    <div className="img_cont">
+                                        <img src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg" className="rounded-circle user_img" />
+                                    </div>
+                                    <div className="user_info">
+                                        <span>{tile.other.name}</span>
+                                    </div>
+                                    <div className="user_button">
+                                        <IconButton edge="end" aria-label="review" style={{ padding: "0px", marginRight: "8px" }} onClick={() => handleOpen(tile.other.id)}>
+                                            <RateReviewIcon />
+                                        </IconButton>
+                                        <IconButton edge="end" aria-label="delete" style={{ padding: "0px" }}>
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ui>
+                </div>
+            </div>
         </div>
     );
+    console.log(state.currChat);
+    if (!$.isEmptyObject(state.currChat)) {
+        return (
+            <ChatWindow chatRoom={state.currChat} currUser={currUser} toggleDrawer={toggleDrawer} state={state} sideList={sideList} modalState={modalState} handleClose={handleClose} modalStyle={modalStyle}/>
+        );
+    } else {
+        return (
+            <div style={{ height: "100vh" }}>
+                <div id="action_menu_btn1">
+                    <IconButton onClick={toggleDrawer("left", true)}>
+                        <ListIcon />
+                    </IconButton>
+                </div>
+                <SwipeableDrawer
+                    open={state.left}
+                    onClose={toggleDrawer("left", false)}
+                    onOpen={toggleDrawer("left", true)}
+                >
+                    {sideList("left")}
+                </SwipeableDrawer>
+                <Modal open={modalState.open} handleClose={handleClose} modalStyle={modalStyle} reviewer={currUser.id} reviewee={modalState.reviewee} />
+            </div>
+        );
+    }
 });
 
 export default Chat;
