4520bdad551ea6c31fc494a2c59e237c92919ace
Carl J. Conrad <carlj.conrad@gmail.com>
2020-03-06 00:02:52 -0800

    search style finished

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,25 +1,13 @@
 import React, { useState } from 'react'
-import Card from 'material-ui/Card'
-import Divider from 'material-ui/Divider'
-// import MenuItem from 'material-ui/Menu/MenuItem'
-import TextField from 'material-ui/TextField'
-import Button from 'material-ui/Button'
 import SearchIcon from 'material-ui-icons/Search'
 import API from "../../utils/API-User";
-// import { createMuiTheme } from 'material-ui/styles'
-// import { useTheme } from '@material-ui/core/styles';
 import Candidates from "../Candidates";
-// import axios from "axios";
 import Fuse from "fuse.js";
 import Select from '../SearchSelect/index'
-import IconButton from '@material-ui/core/IconButton';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
-
-// set a default search value for onMount
-
 
 const Search = ({ category, currUser, setCurrUser }) => {
     let serviceOptions = {
@@ -50,25 +38,8 @@ const Search = ({ category, currUser, setCurrUser }) => {
             "needs"
         ]
     };
-    // const theme = useTheme();
+
     const classes = {
-        card: {
-            margin: 'auto',
-            textAlign: 'center',
-            paddingTop: 10,
-            backgroundColor: '#80808024',
-            border: "groove 1px #96CDFF"
-        },
-        menu: {
-            width: 200,
-        },
-        textField: {
-            marginLeft: "auto",
-            marginRight: "auto",
-            width: 130,
-            verticalAlign: 'bottom',
-            marginBottom: '20px'
-        },
         searchField: {
             marginLeft: "auto",
             marginRight: "auto",
@@ -76,21 +47,11 @@ const Search = ({ category, currUser, setCurrUser }) => {
             marginBottom: '0px',
             marginTop: '4px'
         },
-        searchButton: {
-            width: 'auto',
-            height: '60px',
-            padding: '0 8px',
-            // backgroundColor: "#96CDFF",
-            backgroundImage: "linear-gradient(to right, #96CDFF 0%, #077699 51%, #96CDFF 100%)",
-            border: "solid 1px #077699",
-            borderRadius: "15px",
-            marginBottom: "24px"
-        },
-        // candidates: {
-        //     border: "solid 1px white",
-        //     borderRadius: "20px",
-        //     maxWidth: 400,
-        // }
+        root: {
+            background: "transparent",
+            border: "black"
+        }
+ 
     };
 
     const [state, setState] = useState({
@@ -123,16 +84,7 @@ const Search = ({ category, currUser, setCurrUser }) => {
                         let fuse = new Fuse(data.data, serviceOptions);
                         currResult = fuse.search(state.search);
                         currResult = currResult.filter(user => user._id !== currUser.id);
-                        // data.data.map(user => {
-                        //     console.log(user);
-                        //     if (user._id!==currUser.id) {
-                        //         if (user.offers.includes(state.search)) {
-                        //             currResult.push(user);
-                        //             console.log(currResult);
-                        //         }
-                        //     }
-                        // });
-                        // console.log(currResult);
+ 
                         if (currResult.length === 0) {
                             currResult = data.data.filter(user => user._id !== currUser.id);
                             setState({ ...state, results: currResult });
@@ -145,13 +97,7 @@ const Search = ({ category, currUser, setCurrUser }) => {
                         let fuse = new Fuse(data.data, needOptions);
                         currResult = fuse.search(state.search);
                         currResult = currResult.filter(user => user._id !== currUser.id);
-                        // data.data.map(user => {
-                        //     if (user.id !== currUser.id) {
-                        //         if (user.needs.includes(state.search)) {
-                        //             currResult.push(user);
-                        //         }
-                        //     }
-                        // });
+        
                         if (currResult.length === 0) {
                             currResult = data.data.filter(user => user._id !== currUser.id);
                             setState({ ...state, results: currResult });
@@ -173,18 +119,23 @@ const Search = ({ category, currUser, setCurrUser }) => {
 
 
     return (
-        // <div >
         <React.Fragment>
-            {/* <Card style={classes.card}> */}
+    
             <Select
                 setState={handleChange('category')} category={state.category}
             />
 
             <FormControl style={classes.searchField}>
-                <InputLabel onKeyDown={enterKey} onChange={handleChange('search')} style={classes.searchField}>Search</InputLabel>
+                <InputLabel 
+                onKeyDown={enterKey} 
+                onChange={handleChange('search')} style={classes.searchField}
+                >
+                Search
+                </InputLabel>
                 <Input
                     endAdornment={
-                        <InputAdornment position="end">
+                        <InputAdornment position="end"
+                        >
                             <SearchIcon
                                 onClick={search}
                             >
@@ -193,24 +144,15 @@ const Search = ({ category, currUser, setCurrUser }) => {
                     }
                 />
             </FormControl>
-            {/* <TextField
-                id="search"
-                label="Search products"
-                type="search"
-                onKeyDown={enterKey}
-                onChange={handleChange('search')}
-                style={classes.searchField}
-                margin="normal"
-            /> */}
-            {/* <Button variant="raised" style={classes.searchButton} onClick={search}>
-                <SearchIcon />
-            </Button> */}
-            {/* <Divider /> */}
-            <Candidates category={state.category} candidates={state.results} searched={state.searched} currUser={currUser} setCurrUser={setCurrUser} />
-            {/* </Card> */}
-        </React.Fragment>
-        // </div>
+            <Candidates 
+            category={state.category} 
+            candidates={state.results} 
+            searched={state.searched} 
+            currUser={currUser} 
+            setCurrUser={setCurrUser} 
+            />
 
+        </React.Fragment>
     )
 }
 
