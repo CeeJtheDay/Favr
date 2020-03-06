85a10a63dd072904ee51c87d4e4868ad6253e252
Carl J. Conrad <carlj.conrad@gmail.com>
2020-03-06 00:43:26 -0800

    profile header style finished

--- client/src/components/ProfilePic/index.js
+++ client/src/components/ProfilePic/index.js
@@ -14,7 +14,7 @@ const useStyles = makeStyles(theme => ({
     margin: "20px 35px 20px 35px",
     boxShadow: "2px 4px 2px rgb(43,41,44, 0.3)",
     width: theme.spacing(8),
-    height: theme.spacing(8),
+    height: theme.spacing(8)
   },
 }));
 export default function ImageAvatars({currUser}) {
